e58abd72df5353745b99a0f27ab4ef699881e076
Danny Hilario Suarez <dannyhilariosuarez@gmail.com>
2017-05-23 12:00:07 -0400

    Add files via upload

---
+++ pset3/pset3/find/helpers.c
@@ -0,0 +1,101 @@
+/**
+ * helpers.c
+ *
+ * Computer Science 50
+ * Problem Set 3
+ *
+ * Helper functions for Problem Set 3.
+ */
+       
+       
+
+/**************************************************************************** 
+ * Danny Hilario Suarez
+ ***************************************************************************/
+ 
+ 
+
+#include <cs50.h>
+
+#include "helpers.h"
+
+/**
+ * Returns true if value is in array of n values, else false.
+ */
+bool search(int value, int values[], int n)
+{
+    // TODO: implement a searching algorithm
+    
+    int ending = n-1;
+    int beggining = 0;
+
+    while (ending >= beggining)
+    {
+        int middle = (beggining + ending) / 2;
+        if (values[middle] == value)
+            return true;
+        else if (values[middle] > value)
+            ending = middle - 1;
+        else
+            beggining = middle + 1;
+    }
+
+    return false;
+    
+    return false;
+}
+
+/**
+ * Sorts array of n values.
+ */
+void sort(int values[], int n)
+{
+    // TODO: implement an O(n^2) sorting algorithm
+    int temp;
+    for (int i = 0; i < n; i++)
+    {
+        int smallest_index = i;
+        for (int j = i+1; j < n; j++)
+        {
+            if (values[smallest_index] > values[j])
+                smallest_index = j;
+        }
+        // Swap values;
+        temp = values[smallest_index];
+        values[smallest_index] = values[i];
+        values[i] = temp;
+    return;
+}
+
+// bool linear_search(int value, int values[], int n)
+// {
+//     for (int i = 0; i < n; i++)
+//         if (values[i] == value)
+//             return true;
+
+//     return false;
+// }
+ 
+// // Custom sort functions
+// void bubble_sort(int values[], int n)
+// {
+//     bool iter;
+
+//     do
+//     {
+//         iter = false;
+//         for (int i = 0; i < n-1; i++)
+//         {
+//             if (values[i] > values[i+1])
+//             {
+//                 int temp = values[i];
+//                 values[i] = values[i+1];
+//                 values[i+1] = temp;
+//                 iter = true;
+//             }
+//         }
+//     } while (iter);
+//     return;
+// }
+ 
+}
